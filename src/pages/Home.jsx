cc6e376c126c73008f5c8c4822f96667fc830428
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-08-06 16:21:00 +0200

    create structure for project

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import { ArrowRight, Sparkles, Zap, Shield } from "lucide-react";
+
+export default function Home() {
+    return (
+        <section>
+            <div className="max-w-7xl mx-auto px-4">
+                <div className="inline-flex items-center gap-2 rounded-full bg-primary/10 border border-primary">
+                    <h1>wahou</h1>
+                </div>
+            </div>
+        </section>
+    );
+}
