10867f8414f27b4c3d532311d28b4eea65cae668
Michael <56340359+SynysterRev@users.noreply.github.com>
2025-08-26 17:15:57 +0200

    ad the opening of edit modal from settings qr info

--- src/components/ui/QrInfoModal.tsx
+++ src/components/ui/QrInfoModal.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Modal from "./Modal";
 import QrPreview from "./QrPreview";
 import { formatLocalDate } from "@/lib/utils/utils";
+import { useModals } from "@/app/contexts/ModalContext";
 
 export default function QrInfoModal({
     qr,
@@ -16,7 +17,19 @@ export default function QrInfoModal({
     onClose: () => void;
 }) {
     const [section, setSection] = useState(initialSection);
+    const { openDeleteQrModal, openEditQrModal } = useModals();
     const creationDate = formatLocalDate(qr.createdAt.toString())
+
+    const handleOpenEditModal = (qr: QrData) => {
+        onClose();
+        openEditQrModal(qr);
+    }
+
+    const handleOpenDeleteModal = (qr: QrData) => {
+        onClose();
+        openDeleteQrModal(qr);
+    }
+
     return (
         <>
             <Modal onClose={onClose} isOpen={isOpen} title={qr?.title}>
@@ -77,11 +90,16 @@ export default function QrInfoModal({
                     {section === "settings" && (
                         <div className="flex flex-col gap-2">
                             <button className="px-4 py-2 bg-primary text-white rounded-xl cursor-pointer transition-all group duration-300 hover:shadow-[var(--shadow-elegant)] 
-                    hover:bg-primary/90 bg-gradient-to-r from-primary to-primary-glow">
+                    hover:bg-primary/90 bg-gradient-to-r from-primary to-primary-glow"
+                                onClick={() => handleOpenEditModal(qr)}
+                            >
                                 Edit
                             </button>
-                            <button className="px-4 py-2 bg-red-500 text-white rounded-xl cursor-pointer transition-all group duration-300 hover:shadow-[var(--shadow-elegant)] 
-                    hover:bg-red-500/90 bg-gradient-to-r from-red-500 to-red-glow">
+                            <button
+                                className="px-4 py-2 bg-red-500 text-white rounded-xl cursor-pointer transition-all group duration-300 hover:shadow-[var(--shadow-elegant)] 
+                    hover:bg-red-500/90 bg-gradient-to-r from-red-500 to-red-glow"
+                                onClick={() => handleOpenDeleteModal(qr)}
+                            >
                                 Delete
                             </button>
                         </div>
